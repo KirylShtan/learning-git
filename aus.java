dfc48294b9d80ffe3b246c2b7e677e78f3ec3a0b
Arlou Kiryl <gambrinas13@rambler.ru>
2020-05-28 18:20:13 +0200

    changed auth in conf branch

--- aus.java
+++ aus.java
@@ -1,5 +1,5 @@
 public boolean autorization(String login,String password){
-    if (login.equals("login") && password.equals("password")){
+    if (login.equals("login") || password.equals("password")){
         return true;
         }
     else {
